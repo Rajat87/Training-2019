ef7218f863f06c64d3adffae1bf4f70bdf4d417e
Rajat Pandey <Rajat Pandey@Rajat>
2019-01-17 16:54:38 +0530

    Single Value Containers

---
+++ Session1/src/startdemo.java
@@ -0,0 +1,19 @@
+
+public class startdemo {
+
+	public static void main(String[] args) {
+		//concept of containers
+		byte age=10;//insertion
+		age=12;//updation
+		System.out.println("age is:"+age);//print it or display it
+		double salary=250000.767;
+        System.out.println("salary is:"+salary);
+        char c='A';//character
+        System.out.println("alphabet is"+c);
+        char d='\u00a9';
+        System.out.println("Unicode is:"+d);
+        boolean isinternetconnected=true;
+        System.out.println("Mobile data on?:"+isinternetconnected);
+	}
+
+}
